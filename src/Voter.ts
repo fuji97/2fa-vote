45cc3fb2d44862206d7bd55ead849e1c3899b667
Federico 'Fuji' Rapetti <fuji1097@gmail.com>
2021-08-03 14:26:59 +0200

    Implement Voter cast vote

--- src/Voter.ts
+++ src/Voter.ts
@@ -1,3 +1,37 @@
-class Voter {
+import {KeyPair, PublicParameters, Vote, PublicKey} from "./types";
+import {EddsaSign, generateKeypair, sign, verify} from "./eddsa";
+import {scalarToPoint} from "./babyjubjub";
+import assert from "assert";
 
+export type CastedVote = {
+    vote: Vote;
+    sign: EddsaSign;
+    pubKey: PublicKey;
+}
+
+export class Voter {
+    keypair: KeyPair;
+    publicParameters: PublicParameters;
+    keys: Array<PublicKey>
+
+    constructor(keypair: KeyPair, publicParameters: PublicParameters) {
+        this.keypair = keypair;
+        this.publicParameters = publicParameters;
+
+        this.keys = new Array<PublicKey>();
+    }
+
+    castVote(vote: Vote): CastedVote {
+        const pair = generateKeypair();
+        let sig = sign(vote, pair.privateKey);
+
+        // Check signature
+        assert(verify(vote, pair.publicKey, sig), "Invalid EdDSA MiMC-7 signature");
+
+        return {
+            vote,
+            sign: sig,
+            pubKey: pair.publicKey
+        }
+    }
 }
